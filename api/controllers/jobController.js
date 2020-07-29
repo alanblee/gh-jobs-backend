e9ba9cf533725d9b8b196c179783a61ef7ffc06b
alanblee <alanbenlee12@gmail.com>
2020-07-29 13:08:30 -0700

    added DELETE controller func

--- api/controllers/jobController.js
+++ api/controllers/jobController.js
@@ -2,8 +2,9 @@ const Jobs = require("./helpers/jobHelper");
 
 // GET - gets all saved jobs
 module.exports.getAllJobs = async (req, res) => {
+  let userId = Number(req.user.id);
   try {
-    const allJobs = await Jobs.getJobs();
+    const allJobs = await Jobs.getJobs(userId);
     if (allJobs.length) {
       res.status(200).json(allJobs);
     } else {
@@ -15,3 +16,27 @@ module.exports.getAllJobs = async (req, res) => {
 };
 
 // DELETE
+module.exports.deleteJob = async (req, res) => {
+  const jobId = Number(req.params.id);
+  const userId = Number(req.user.id);
+  try {
+    const [foundJob] = Jobs.findById(jobId);
+    if (!foundJob) {
+      res.status(404).json({ message: "Cannot find job with that id" });
+    }
+    if (Number(foundJob.user_id) === userId) {
+      const [deletedJob] = Jobs.deleteJob(jobId);
+      if (deletedJob) {
+        res
+          .status(200)
+          .json({ message: `Job deleted with id of ${deletedJob}` });
+      } else {
+        res.status(404).json({ message: "Job not found with that ID" });
+      }
+    } else {
+      res.status(404).json({ message: "Cannpt find job with that id" });
+    }
+  } catch (err) {
+    res.status(500).json({ message: "An error has occured", err: err.message });
+  }
+};
